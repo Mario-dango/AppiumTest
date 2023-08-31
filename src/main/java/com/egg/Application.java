2ae2835d1de8b82a770a3f74e08a8146b76b56b2
Mario Papetti <mp.robots@gmail.com>
2023-08-30 21:53:43 -0300

    primera prueba funcional

---
+++ src/main/java/com/egg/Application.java
@@ -0,0 +1,8 @@
+package com.egg;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/egg/AppiumFirefoxTest.java
@@ -0,0 +1,48 @@
+package com.egg;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumFirefoxTest {
+
+    @Test
+    public void asdasd() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        // Configuración para Android
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "b6e068160409"); // Cambiar a nombre de tu dispositivo
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+        // // Inicializa la conexión con el dispositivo a través de USB
+        // capabilities.setCapability(MobileCapabilityType.UDID, "udid_del_dispositivo"); // Cambiar a UDID de tu dispositivo
+
+        URL appiumServerURL = null;
+        try {
+            appiumServerURL = new URL("http://localhost:4723");
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        AppiumDriver<MobileElement> driver = new AndroidDriver<>(appiumServerURL, capabilities);
+
+        // Abre una URL en el navegador Firefox
+        driver.get("https://www.google.com");
+
+        // Realiza alguna interacción adicional, como hacer clic en enlaces o buscar información
+        // ...
+
+        // Cierra la sesión
+        driver.quit();
+    }
+}
